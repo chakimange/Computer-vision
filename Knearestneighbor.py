5034f8f133ea3a0bfe8a273e8429beb97f959887
Xavier <xavier.cordonnier10@gmail.com>
2025-10-12 13:45:49 +0900

    knn with cross validation -fold

--- Knearestneighbor.py
+++ Knearestneighbor.py
@@ -1,26 +1,22 @@
-import pandas as pd
-import numpy as np
-import cv2
-import os
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, KFold,train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
 # ETAPE 1 : Charger CIFAR-10 officiel
-(X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
+(X_train_e, y_train_e), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-X_train_split, X_val, y_train_split, y_val = train_test_split(X_train, y_train ,test_size=0.2 ,random_state=42,stratify=y_train)
-
-print(f"Train: {len(X_train)} images")
+print(f"Train: {len(X_train_e)} images")
 print(f"Test: {len(X_test)} images")
 
-y_train = y_train.flatten()
+y_train_e = y_train_e.flatten()
 y_test = y_test.flatten()
 
+X_train, X_val, y_train, y_val = train_test_split(X_train_e, y_train_e,test_size = 0.2, random_state=42, stratify = y_train_e)
+
 # ETAPE 2 : Préparer les données 
-X_train_normalise = X_train_split.astype('float32') / 255.0
+X_train_normalise = X_train.astype('float32') / 255.0
 X_val_normalise = X_val.astype('float32') / 255.0
 X_test_normalise = X_test.astype('float32') / 255.0
 
@@ -29,38 +25,38 @@ X_train_flat = X_train_normalise.reshape(len(X_train_normalise), -1)
 X_val_flat = X_val_normalise.reshape(len(X_val_normalise), -1)
 X_test_flat = X_test_normalise.reshape(len(X_test_normalise), -1)
 
-print(f"Forme données: {X_train_flat.shape}")
 
-# Noms des classes CIFAR-10
 class_names = ['avion', 'voiture', 'oiseau', 'chat', 'cerf', 
                'chien', 'grenouille', 'cheval', 'bateau', 'camion']
 
+f1_score_list=[]
+
 # Entraîner k-NN
-knn = KNeighborsClassifier(n_neighbors=3, weights="distance")
-knn.fit(X_train_flat, y_train_split)
-print("Modèle entraîné!")
+for k in range(1,20):
+    knn = KNeighborsClassifier(n_neighbors=k, weights="distance")
+    knn.fit(X_train_flat, y_train)
 
 
-# ETAPE 4 : Evaluer sur la validation
-val_predictions = knn.predict(X_val_flat)
-val_accuracy = accuracy_score(y_val, val_predictions)
-val_precision = precision_score(y_val, val_predictions, average='weighted')
-val_recall = recall_score(y_val, val_predictions, average='weighted')
-val_f1 = f1_score(y_val, val_predictions, average='weighted')
+    # ETAPE 3 : Evaluer sur la validation croisée
+    kfold = KFold(n_splits=5, shuffle=True, random_state=42)
+    cv_accuracy = cross_val_score(knn, X_train_flat, y_train, cv=kfold, scoring='accuracy', n_jobs=-1)
+    cv_precision = cross_val_score(knn, X_train_flat, y_train, cv=kfold, scoring='precision_weighted', n_jobs=-1)
+    cv_recall = cross_val_score(knn, X_train_flat, y_train, cv=kfold, scoring='recall_weighted', n_jobs=-1)
+    cv_f1 = cross_val_score(knn, X_train_flat, y_train, cv=kfold, scoring='f1_weighted', n_jobs=-1)
 
-print(f"Accuracy (validation): {val_accuracy:.3f}")
-print(f"Précision (validation): {val_precision:.3f}")
-print(f"Recall (validation): {val_recall:.3f}")
-print(f"F1 (validation): {val_f1:.3f}")
+    """print(f"Accuracy moyenne: {cv_accuracy.mean():.3f}")
+    print(f"Précision moyenne: {cv_precision.mean():.3f}")
+    print(f"Recall moyen: {cv_recall.mean():.3f}")
+    print(f"F1 moyen: {cv_f1.mean():.3f}")"""
+    print(k)
+    f1_score_list.append(cv_f1.mean())
+    
+    
+plt.plot(range(1,20),f1_score_list)
+plt.xlabel("k")
+plt.ylabel("F1 score")
+plt.title("PLot of the accuracy with respect to k")
+plt.show()
 
-# ETAPE 3 : Evaluer
-predictions = knn.predict(X_test_flat)
-accuracy = accuracy_score(y_test, predictions)
-precision = precision_score(y_test, predictions, average='weighted')  
-recall = recall_score(y_test, predictions, average='weighted')        
-f1 = f1_score(y_test, predictions, average='weighted')               
-
-print(f"Accuracy: {accuracy:.3f}")
-print(f"Précision: {precision:.3f}")
-print(f"Recall: {recall:.3f}")
-print(f"F1: {f1:.3f}")
+#starts running at 9h30
+#finished running at 13h50
